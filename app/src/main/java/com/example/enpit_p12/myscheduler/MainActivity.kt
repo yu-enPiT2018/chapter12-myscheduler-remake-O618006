6c16b9646c490601df93b6c4046fdb2b936b3150
enPiT-P30 <d806@oshima-k.ac.jp>
2018-09-18 18:10:21 +0900

    完成

--- app/src/main/java/com/example/enpit_p12/myscheduler/MainActivity.kt
+++ app/src/main/java/com/example/enpit_p12/myscheduler/MainActivity.kt
@@ -5,20 +5,39 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import io.realm.Realm
+import io.realm.kotlin.where
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var realm: Realm
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        realm = Realm.getDefaultInstance()
+        val schedules = realm.where<Schedule>().findAll()
+        listView.adapter = ScheduleAdapter(schedules)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            //        .setAction("Action", null).show()
+            startActivity<ScheduleEditActivity>()
         }
+
+        listView.setOnItemClickListener{ parent, view ,position,id ->
+            val schedule = parent.getItemAtPosition(position) as Schedule
+            startActivity<ScheduleEditActivity>("schedule_id" to schedule.id)
+        }
+    }
+
+    override  fun onDestroy(){
+        super.onDestroy()
+        realm.close()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
